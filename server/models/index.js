6896165ea44633a7552b3ef11b7595a33a7f0e2d
josephn90 <j.ndububa@gmail.com>
2021-09-16 22:03:58 -0400

    added record to model index

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 // import the models to package all of them together
 const User = require('./User');
+const Record = require('./Record');
 
 // export the models
-module.exports={User};
+module.exports={ User, Record };
