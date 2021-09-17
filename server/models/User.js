e1e713fd98d97b48d79dde68cbabdeae3f1b9524
josephn90 <j.ndububa@gmail.com>
2021-09-16 22:05:52 -0400

    added bcrypt to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 // import the Schema constructor and the model function from the mongoose library
 const {Schema, model}=require('mongoose');
+const bcrypt = require('bcrypt');
 
 // import RecordSchema from Record.js
 const RecordSchema = require('./Record');
@@ -22,6 +23,21 @@ const UserSchema = new Schema({
     },
     savedRecords:[RecordSchema]
 });
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 // Create the User model using UserSchema
 const User = model('User',UserSchema);
