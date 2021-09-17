21e5c7691a0d60d357bd0bfcc34910e0ab896b1d
josephn90 <j.ndububa@gmail.com>
2021-09-16 22:11:02 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,45 @@
-oiewfhoifjrewfrjewpoi
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+let server;
+
+async function startServer() {
+  // create Apollo Server and pass in our schema
+  server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  // Integrate our Appollo server with the express application as middleware
+  server.applyMiddleware({ app });
+}
+
+// start the server
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
