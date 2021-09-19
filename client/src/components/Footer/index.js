13a946aba91c5039bdcb203d779b10e919c60598
Mayuran <mayuranswee23@gmail.com>
2021-09-19 14:32:56 -0400

    first commits for frontend

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>
+                2021 by Group 1
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
