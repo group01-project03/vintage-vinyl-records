84882961275b04f66359e01b04150563131a6810
Amna Syeda <amna.syeda98@gmail.com>
2021-09-20 19:55:04 -0400

    added @emotion/styled for css

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+import styled from '@emotion/styled'; 
 
 import { QUERY_RECORDS } from "../utils/queries";
 
