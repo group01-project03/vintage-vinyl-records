2061193ba75581995f56d225e1de9538a9cb2702
Amna Syeda <amna.syeda98@gmail.com>
2021-09-23 12:04:10 -0400

    pushing changed constants

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/vintage-vinyl-records',{
-    useFindAndModify: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/vintage-vinyl-records', {
+   // useFindAndModify: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+   // useCreateIndex: true,
 });
 
 // to log mongo queries being executed!
-mongoose.set('debug',true);
+//mongoose.set('debug',true);
 
 module.exports = mongoose.connection;
