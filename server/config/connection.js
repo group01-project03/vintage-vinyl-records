a702a42b4e2346f5323cc7ae0f3628536dda3758
Amna Syeda <amna.syeda98@gmail.com>
2021-09-23 12:29:47 -0400

    changed start of package.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,6 +8,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/vintage-vinyl-r
 });
 
 // to log mongo queries being executed!
-//mongoose.set('debug',true);
+mongoose.set('debug',true);
 
 module.exports = mongoose.connection;
